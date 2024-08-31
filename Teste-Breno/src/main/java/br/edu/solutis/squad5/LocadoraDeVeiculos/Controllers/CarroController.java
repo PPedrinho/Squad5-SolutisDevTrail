ae958eaa00451eab375c2038c1b1da34d8f5fca7
Breno Canguçu <110245827+brenocangucu@users.noreply.github.com>
2024-08-31 14:34:15 -0300

    Delete Teste-Breno directory

--- Teste-Breno/src/main/java/br/edu/solutis/squad5/LocadoraDeVeiculos/Controllers/CarroController.java
+++
@@ -1,4 +0,0 @@
-package br.edu.solutis.squad5.LocadoraDeVeiculos.Controllers;
-
-public class CarroController {
-}
--- Teste-Breno/src/main/java/br/edu/solutis/squad5/LocadoraDeVeiculos/Repository/VeiculosRepository.java
+++
@@ -1,4 +0,0 @@
-package br.edu.solutis.squad5.LocadoraDeVeiculos.Repository;
-
-public class VeiculosRepository {
-}
